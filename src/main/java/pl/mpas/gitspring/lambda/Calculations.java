6899ed8b897e48c58492fd0fe8756b258815f750
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-14 14:45:14 +0100

    [change] lambda

--- src/main/java/pl/mpas/gitspring/lambda/Calculations.java
+++ src/main/java/pl/mpas/gitspring/lambda/Calculations.java
@@ -2,5 +2,5 @@ package pl.mpas.gitspring.lambda;
 
 public interface Calculations {
     int calc(int a, int b);
-
+   default void calc2() {}
 }
---
+++ src/main/java/pl/mpas/gitspring/lambda/nic.java
@@ -0,0 +1,5 @@
+package pl.mpas.gitspring.lambda;
+
+public interface nic {
+    String metoda(String i, String n, int w);
+}
