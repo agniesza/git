70812dea83cf4daec2c969469d85716ad0e351fb
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-14 12:31:39 +0100

    [init] - initial commit

---
+++ src/main/java/pl/mpas/gitspring/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package pl.mpas.gitspring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(path = "/")
+    //  @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String home(Model model) {
+        model.addAttribute("message", "Hello World!!!");
+        return "home";
+    }
+
+}
---
+++ src/main/resources/templates/home.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html xmlns:th="http://www.thymeleaf.org">
+
+<html>
+<head>
+    <title>HOME</title>
+</head>
+<body class="card">
+
+<div id="main">
+    Home page
+</div>
+</body>
+
+</html>
+
