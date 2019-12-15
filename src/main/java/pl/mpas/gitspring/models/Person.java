f25e6a8cd629493469218bc39d9262e34782417e
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-15 08:21:54 +0100

    [edit] - person

--- src/main/java/pl/mpas/gitspring/models/Person.java
+++ src/main/java/pl/mpas/gitspring/models/Person.java
@@ -1,39 +1,40 @@
 package pl.mpas.gitspring.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.tomcat.jni.Address;
+@Data
+@AllArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
     private int age;
+    private Address address;
 
 public Person(){}
 
-    public Person(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  //  public void setFirstName(String firstName) {
+   //     this.firstName = firstName;
+    //}
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    //public void setLastName(String lastName) {
+      //  this.lastName = lastName;
+    //}
 
     public int getAge() {
         return age;
     }
 
-    public void setAga(int age) {
-        this.age = age;
-    }
+    //public void setAga(int age) {
+     //   this.age = age;
+    //}
 }
