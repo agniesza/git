d920475456ed6bc2e050094001fdda4e2ecb192c
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-15 08:11:44 +0100

    [change] -

--- src/main/java/pl/mpas/gitspring/optional/OptionalExample.java
+++ src/main/java/pl/mpas/gitspring/optional/OptionalExample.java
@@ -1,5 +1,24 @@
 package pl.mpas.gitspring.optional;
 
+import java.util.Optional;
+
 public class OptionalExample {
-    int a;
+    public static void main(String[] args){
+        Optional <String> maybeNick=findNick();
+
+if(maybeNick.isPresent()){
+
+    System.out.println(maybeNick.get());
+}
+//nowy sposob lambda
+    maybeNick.ifPresent(
+            nick -> System.out.println("mam nick"+nick)
+    );
+
+    }
+    private static Optional<String> findNick(){
+        return Optional.empty();
+    }
+
+
 }
